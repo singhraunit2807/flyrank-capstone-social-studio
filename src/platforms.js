9ac4758a7451d272092f03bc0d41afc325fba531
Raunit Singh <baburajasingh7484@gmail.com>
2026-08-28 15:48:23 +0530

    Fix platform module for ESM and publisher abstraction

--- src/platforms.js
+++ src/platforms.js
@@ -1,30 +1,36 @@
-const PLATFORM_SPECS = {
+export const PLATFORM_SPECS = {
   instagram: { width: 1080, height: 1080, aspectRatio: '1:1' },
-  x: { width: 1600, height: 900, aspectRatio: '16:9' },
+  x: { width: 1600, height: 900, aspectRatio: '16:9' }
 };
 
-class SocialPublisher {
-  async publish(_post) { throw new Error('publish() must be implemented'); }
+export class SocialPublisher {
+  async publish(_post) {
+    throw new Error('publish() must be implemented');
+  }
 }
 
-class FakePlatformPublisher extends SocialPublisher {
-  constructor(platform, client) { super(); this.platform = platform; this.client = client; }
+export class FakePlatformPublisher extends SocialPublisher {
+  constructor(platform, client) {
+    super();
+    this.platform = platform;
+    this.client = client;
+  }
+
   async publish(post) {
     return this.client.publish({ platform: this.platform, ...post });
   }
 }
 
-class FakeInstagramPublisher extends FakePlatformPublisher {
+export class FakeInstagramPublisher extends FakePlatformPublisher {
   constructor(client) { super('instagram', client); }
 }
-class FakeXPublisher extends FakePlatformPublisher {
+
+export class FakeXPublisher extends FakePlatformPublisher {
   constructor(client) { super('x', client); }
 }
 
-function getPublisher(platform, client) {
+export function getPublisher(platform, client) {
   if (platform === 'instagram') return new FakeInstagramPublisher(client);
   if (platform === 'x') return new FakeXPublisher(client);
   throw new Error(`Unsupported platform: ${platform}`);
 }
-
-module.exports = { PLATFORM_SPECS, SocialPublisher, FakeInstagramPublisher, FakeXPublisher, getPublisher };
